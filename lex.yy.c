a483ca45bbc0cf5abdd0fcb9e8b217f29740b4ca
Jabedweb <jabedhasan231@gmail.com>
2023-12-29 01:01:12 +0600

    add Read me file

--- lex.yy.c
+++ lex.yy.c
@@ -261,9 +261,6 @@ static void yy_flex_free YY_PROTO(( void * ));
 
 #define YY_AT_BOL() (yy_current_buffer->yy_at_bol)
 
-
-#define yywrap() 1
-#define YY_SKIP_YYWRAP
 typedef unsigned char YY_CHAR;
 FILE *yyin = (FILE *) 0, *yyout = (FILE *) 0;
 typedef int yy_state_type;
@@ -285,30 +282,30 @@ static void yy_fatal_error YY_PROTO(( yyconst char msg[] ));
 	*yy_cp = '\0'; \
 	yy_c_buf_p = yy_cp;
 
-#define YY_NUM_RULES 12
-#define YY_END_OF_BUFFER 13
-static yyconst short int yy_accept[20] =
+#define YY_NUM_RULES 4
+#define YY_END_OF_BUFFER 5
+static yyconst short int yy_accept[12] =
     {   0,
-        0,    0,   13,   12,    1,    4,   10,    8,    9,   11,
-        2,    7,    6,    3,    1,    2,    5,    3,    0
+        0,    0,    5,    4,    1,    2,    1,    3,    2,    3,
+        0
     } ;
 
 static yyconst int yy_ec[256] =
     {   0,
-        1,    1,    1,    1,    1,    1,    1,    1,    2,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    2,    1,    1,    1,    1,    3,    1,    1,    1,
-        1,    4,    5,    1,    6,    1,    7,    8,    8,    8,
-        8,    8,    8,    8,    8,    8,    8,    1,    9,    1,
-       10,    1,    1,    1,   11,   11,   11,   11,   11,   11,
-       11,   11,   11,   11,   11,   11,   11,   11,   11,   11,
-       11,   11,   11,   11,   11,   11,   11,   11,   11,   11,
-        1,    1,    1,    1,   12,    1,   11,   11,   11,   11,
+        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    1,    1,    2,    1,    1,    1,    3,    3,    3,
+        3,    3,    3,    3,    3,    3,    3,    1,    1,    1,
+        1,    1,    1,    1,    4,    5,    5,    5,    4,    5,
+        5,    5,    4,    5,    5,    5,    5,    5,    4,    5,
+        5,    5,    5,    5,    4,    5,    5,    5,    5,    5,
+        1,    1,    1,    1,    1,    1,    4,    5,    5,    5,
 
-       11,   11,   11,   11,   11,   11,   11,   11,   11,   11,
-       11,   11,   11,   11,   11,   11,   11,   11,   11,   11,
-       11,   11,    1,    1,    1,    1,    1,    1,    1,    1,
+        4,    5,    5,    5,    4,    5,    5,    5,    5,    5,
+        4,    5,    5,    5,    5,    5,    4,    5,    5,    5,
+        5,    5,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
@@ -325,38 +322,33 @@ static yyconst int yy_ec[256] =
         1,    1,    1,    1,    1
     } ;
 
-static yyconst int yy_meta[13] =
+static yyconst int yy_meta[6] =
     {   0,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1
+        1,    1,    2,    3,    3
     } ;
 
-static yyconst short int yy_base[20] =
+static yyconst short int yy_base[14] =
     {   0,
-        0,    0,   21,   22,   18,   22,   22,   22,   22,   22,
-       11,   22,    8,    5,   13,    6,   22,    0,   22
+        0,    0,    8,    9,    9,    0,    0,    0,    0,    0,
+        9,    5,    3
     } ;
 
-static yyconst short int yy_def[20] =
+static yyconst short int yy_def[14] =
     {   0,
-       19,    1,   19,   19,   19,   19,   19,   19,   19,   19,
-       19,   19,   19,   19,   19,   19,   19,   14,    0
+       11,    1,   11,   11,   11,   12,   13,   13,   12,   13,
+        0,   11,   11
     } ;
 
-static yyconst short int yy_nxt[35] =
+static yyconst short int yy_nxt[15] =
     {   0,
-        4,    5,    6,    7,    8,    9,   10,   11,   12,   13,
-       14,   14,   18,   16,   15,   18,   18,   17,   16,   15,
-       19,    3,   19,   19,   19,   19,   19,   19,   19,   19,
-       19,   19,   19,   19
+        4,    5,    6,    7,    8,   10,    9,   11,    3,   11,
+       11,   11,   11,   11
     } ;
 
-static yyconst short int yy_chk[35] =
+static yyconst short int yy_chk[15] =
     {   0,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,   14,   16,   15,   14,   14,   13,   11,    5,
-        3,   19,   19,   19,   19,   19,   19,   19,   19,   19,
-       19,   19,   19,   19
+        1,    1,    1,    1,    1,   13,   12,    3,   11,   11,
+       11,   11,   11,   11
     } ;
 
 static yy_state_type yy_last_accepting_state;
@@ -370,11 +362,11 @@ static char *yy_last_accepting_cpos;
 #define YY_MORE_ADJ 0
 #define YY_RESTORE_YY_MORE_OFFSET
 char *yytext;
-#line 1 "cal.l"
+#line 1 "CheckChar.lex"
 #define INITIAL 0
-#line 3 "cal.l"
-   #include "cal.tab.h"
-#line 378 "lex.yy.c"
+#line 2 "CheckChar.lex"
+#include <stdio.h>
+#line 370 "lex.yy.c"
 
 /* Macros after this point can all be overridden by user definitions in
  * section 1.
@@ -525,9 +517,10 @@ YY_DECL
 	register char *yy_cp, *yy_bp;
 	register int yy_act;
 
-#line 12 "cal.l"
+#line 5 "CheckChar.lex"
 
-#line 531 "lex.yy.c"
+
+#line 524 "lex.yy.c"
 
 	if ( yy_init )
 		{
@@ -578,13 +571,13 @@ yy_match:
 			while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 				{
 				yy_current_state = (int) yy_def[yy_current_state];
-				if ( yy_current_state >= 20 )
+				if ( yy_current_state >= 12 )
 					yy_c = yy_meta[(unsigned int) yy_c];
 				}
 			yy_current_state = yy_nxt[yy_base[yy_current_state] + (unsigned int) yy_c];
 			++yy_cp;
 			}
-		while ( yy_base[yy_current_state] != 22 );
+		while ( yy_base[yy_current_state] != 9 );
 
 yy_find_action:
 		yy_act = yy_accept[yy_current_state];
@@ -612,65 +605,31 @@ do_action:	/* This label is used only to access EOF actions. */
 
 case 1:
 YY_RULE_SETUP
-#line 13 "cal.l"
-{}
+#line 7 "CheckChar.lex"
+{
+    printf("%s is a vowel.\n", yytext);
+}
 	YY_BREAK
 case 2:
 YY_RULE_SETUP
-#line 14 "cal.l"
-{yylval = atoi(yytext); return(NUM);}
+#line 11 "CheckChar.lex"
+{
+    printf("%s is a digit.\n", yytext);
+}
 	YY_BREAK
 case 3:
 YY_RULE_SETUP
-#line 15 "cal.l"
-{return(ID);}
+#line 15 "CheckChar.lex"
+{
+    printf("%s is a word.\n", yytext);
+}
 	YY_BREAK
 case 4:
 YY_RULE_SETUP
-#line 16 "cal.l"
-{return(MODD);}
-	YY_BREAK
-case 5:
-YY_RULE_SETUP
-#line 17 "cal.l"
-{return(EQ);}
-	YY_BREAK
-case 6:
-YY_RULE_SETUP
-#line 18 "cal.l"
-{return(ASSIGN);}
-	YY_BREAK
-case 7:
-YY_RULE_SETUP
-#line 19 "cal.l"
-{return(SEMI);}
-	YY_BREAK
-case 8:
-YY_RULE_SETUP
-#line 20 "cal.l"
-{ return(ADD); }
-	YY_BREAK
-case 9:
-YY_RULE_SETUP
-#line 21 "cal.l"
-{return(SUB); }
-	YY_BREAK
-case 10:
-YY_RULE_SETUP
-#line 22 "cal.l"
-{ return(MUL); }
-	YY_BREAK
-case 11:
-YY_RULE_SETUP
-#line 23 "cal.l"
-{return(DIV); }
-	YY_BREAK
-case 12:
-YY_RULE_SETUP
-#line 24 "cal.l"
+#line 19 "CheckChar.lex"
 ECHO;
 	YY_BREAK
-#line 674 "lex.yy.c"
+#line 633 "lex.yy.c"
 case YY_STATE_EOF(INITIAL):
 	yyterminate();
 
@@ -962,7 +921,7 @@ static yy_state_type yy_get_previous_state()
 		while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 			{
 			yy_current_state = (int) yy_def[yy_current_state];
-			if ( yy_current_state >= 20 )
+			if ( yy_current_state >= 12 )
 				yy_c = yy_meta[(unsigned int) yy_c];
 			}
 		yy_current_state = yy_nxt[yy_base[yy_current_state] + (unsigned int) yy_c];
@@ -997,11 +956,11 @@ yy_state_type yy_current_state;
 	while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 		{
 		yy_current_state = (int) yy_def[yy_current_state];
-		if ( yy_current_state >= 20 )
+		if ( yy_current_state >= 12 )
 			yy_c = yy_meta[(unsigned int) yy_c];
 		}
 	yy_current_state = yy_nxt[yy_base[yy_current_state] + (unsigned int) yy_c];
-	yy_is_jam = (yy_current_state == 19);
+	yy_is_jam = (yy_current_state == 11);
 
 	return yy_is_jam ? 0 : yy_current_state;
 	}
@@ -1556,4 +1515,38 @@ int main()
 	return 0;
 	}
 #endif
-#line 24 "cal.l"
+#line 19 "CheckChar.lex"
+
+
+int main(void) {
+    yylex();
+    return 0;
+}
+
+int yywrap() {
+    return 1;
+}
+
+/* flex CheckChar.lex
+gcc lex.yy.c
+./a
+*/
+
+
+/*
+Go to Terminal -> Configure Tasks -> Create tasks.json file from template -> Others. This will create a tasks.json file.
+{
+    "version": "2.0.0",
+    "tasks": [
+       {
+          "label": "Build and Run Flex",
+          "type": "shell",
+          "command": "flex ${fileBasename} && gcc lex.yy.c && ./a",
+          "group": {
+             "kind": "build",
+             "isDefault": true
+          }
+       }
+    ]
+ }
+*/
